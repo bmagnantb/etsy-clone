8e7d12528ff1e39656905defeb1fd1e26a492313
bmagnantb <bmagnantb@gmail.com>
2015-02-01 17:08:38 -0600

    search added

--- js/etsy.js
+++ js/etsy.js
@@ -3,8 +3,9 @@
 
     function Etsy() {
         this.recentItems = [];
-        this.apiKey = 'callback=?&api_key=6990q3hh09orsy6z1x0ghq2q';
+        this.apiKey = '&callback=?&api_key=6990q3hh09orsy6z1x0ghq2q';
         this.apiUrlStart = 'https://openapi.etsy.com/v2/';
+        this.prevQuery = '';
 
 
 
@@ -13,19 +14,31 @@
             routes: {
                 '': 'index',
                 ':listing': 'details',
-                'search/:query': 'search'
+                'search/:query': 'drawSearch'
             },
 
             index: function() {
-                self.drawListings(self.urls.interesting, self.templates.listings);
+                self.drawListings(self.urls.interesting, self.templates.listings, '.items');
             },
 
-            search: function() {
-                self.search();
+            drawSearch: function(query) {
+                console.log(query.indexOf(' '));
+                // if (query.indexOf(' ') > -1) {
+                //     query = [].map.call(query, function(val) {
+                //         if (val === ' ') {
+                //             console.log('mapping');
+                //             val = '%20';
+                //         }
+                //         console.log(val);
+                //         return val;
+                //     });
+                query = query.replace(/ /g, '%20');
+                document.location.hash = query;
+                self.drawSearch(self.urls.search, self.templates.listings, '.items', query);
             },
 
             details: function(query, page) {
-                document.querySelector('.items').innerHTML = '';
+
             },
 
             initialize: function() {
@@ -39,7 +52,8 @@
 
     Etsy.prototype = {
         urls: {
-            interesting: 'listings/interesting.js?includes=Images&'
+            interesting: 'listings/interesting.js?includes=Images',
+            search: 'listings/active.js?includes=Images&keywords='
 
         },
 
@@ -47,47 +61,19 @@
             listings: './templates/items.html'
         },
 
-        events: function() {
-            $('#search').on('keydown', function(e) {
-              if(e.keyCode === 13 && this.value !== '') {
-              	window.location.hash = 'search/'+this.value;
-              	this.value = '';
-              }
-            });
-        },
-
-        makeUrl: function(api, key, uri) {
-            return api + uri + key;
-        },
-
-        drawListings: function(url, templateFile) {
-            var self = this;
-            $.when(
-                this.data(url),
-                this.template(templateFile)
-            ).then(function(data, template) {
-                console.log(data, template);
-                template = _.template(template);
-                document.querySelector('.items').innerHTML = template({
-                    data: data
-                });
-            }).then(function(data) {
-                document.querySelector('.items').style.opacity = 1;
-            })
-        },
-
-        data: function(url) {
+        data: function(url, query) {
             var self = this;
             var x = $.Deferred();
             if (this.recentItems.length > 0) {
                 console.log('no download!');
                 x.resolve(this.recentItems);
             } else {
+                console.log(url + ' before download');
                 console.log('download');
-                $.getJSON(this.makeUrl(this.apiUrlStart, this.apiKey, url)).then(function(a) {
+                console.log(url);
+                $.getJSON(this.makeUrl(this.apiUrlStart, this.apiKey, url, query)).then(function(a) {
                     var y = a.results;
                     x.resolve(y);
-                    console.log(self);
                     self.recentItems = y;
                 });
             }
@@ -100,13 +86,60 @@
             });
         },
 
-        search: function() {
+        draw: function(data, template, selector) {
+            console.log(data);
+            console.log(template);
+            template = _.template(template);
+            document.querySelector('.items').innerHTML = template({
+                data: data
+            });
+        },
 
+        events: function() {
+            $('#search').on('keydown', function(e) {
+                if (e.keyCode === 13 && this.value !== '') {
+                    window.location.hash = 'search/' + this.value;
+                    this.value = '';
+                }
+            });
+        },
+
+        makeUrl: function(api, key, uri, query) {
+            query = (typeof query === 'undefined') ? '' : query;
+            console.log(api + uri + query + key);
+            return api + uri + query + key;
+        },
+
+        drawListings: function(url, templateFile, selector) {
+            var self = this;
+            $.when(
+                this.data(url),
+                this.template(templateFile)
+            ).then(function(data, template) {
+                self.draw(data, template, selector)
+            }).then(function(data) {
+                document.querySelector('.items').style.opacity = 1;
+            });
+        },
+
+        drawSearch: function(url, templateFile, selector, query) {
+            document.querySelector('.items').style.opacity = 0;
+
+            var self = this;
+            this.recentItems = [];
+            $.when(
+                this.data(url, query),
+                this.template(templateFile)
+            ).then(function(data, template) {
+                self.draw(data, template, selector)
+            }).then(function(data) {
+                document.querySelector('.items').style.opacity = 1;
+            });
         },
 
         details: function() {
 
-        }
+        },
 
     }
 
